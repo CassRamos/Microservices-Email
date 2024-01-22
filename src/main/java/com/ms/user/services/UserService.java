6965e6eccd3b38c2f2b881207a23cbfa48f99b3e
Cass <cassianoleaoramos@gmail.com>
2024-01-21 22:32:35 -0300

    Inject producer in service class

--- src/main/java/com/ms/user/services/UserService.java
+++ src/main/java/com/ms/user/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ms.user.services;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
 import com.ms.user.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -8,13 +9,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     final UserRepository userRepo;
+    final UserProducer userProducer;
 
-    public UserService(UserRepository userRepo) {
+    public UserService(UserRepository userRepo, UserProducer userProducer) {
         this.userRepo = userRepo;
+        this.userProducer = userProducer;
     }
 
     @Transactional
-    public UserModel save(UserModel userModel) {
-        return userRepo.save(userModel);
+    public UserModel save(UserModel userModel){
+        userModel = userRepo.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
     }
 }
